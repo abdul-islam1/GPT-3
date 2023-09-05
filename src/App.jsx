7c2e838862c79525354e50fd812fcdebc8c666b6
Abdul Islam <130133836+abdul-islam1@users.noreply.github.com>
2023-09-05 22:55:02 +0500

    complete setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import React from "react";
 
-import React from 'react'
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./conttainers";
 
-
+import { CTA, Brand, Navbar } from "./components";
 
 function App() {
-  return (
-    <div>App</div>
-  )
+  return <div>App</div>;
 }
 
-export default App
+export default App;
