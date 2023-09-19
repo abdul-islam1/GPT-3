406a09d0e8418a8121ee9f95ef4a24821c9af3e6
Abdul Islam <130133836+abdul-islam1@users.noreply.github.com>
2023-09-19 14:58:56 +0500

    complete navbar, header. work on whatGPT3 section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./whatGPT3.css";
+import { Feature } from "../../components";
 
 function WhatGPT3() {
-  return <div>WhatGPT3</div>;
+  return <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+
+    <div className="gpt3__whatgpt3-feature">
+      <Feature />
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
+  </div>;
 }
 
 export default WhatGPT3;
